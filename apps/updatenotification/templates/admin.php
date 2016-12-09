ceecba9815e8c7bc3cfc0f54ea0f6e1e97aa7387
Tom Needham <needham.thomas@gmail.com>
2016-12-09 21:02:44 +0000

    Add storage section and fix files_external personal panel

--- apps/updatenotification/templates/admin.php
+++ apps/updatenotification/templates/admin.php
@@ -14,7 +14,7 @@
 	$currentChannel = $_['currentChannel'];
 ?>
 <form id="oca_updatenotification_section" class="section">
-	<h2><?php p($l->t('Updater')); ?></h2>
+	<h2 class="app-name"><?php p($l->t('Updater')); ?></h2>
 
 	<?php if($isNewVersionAvailable === true): ?>
 		<strong><?php p($l->t('A new version is available: %s', [$newVersionString])); ?></strong>
